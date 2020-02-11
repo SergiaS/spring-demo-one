81a236369d5b1e8ab401f1bccd74a2908a9e995d
sergias <sergias@ukr.net>
2020-02-11 18:02:39 +0200

    Practice 3

---
+++ src/main/java/com/luv2code/springdemo/practice3/Coach.java
@@ -0,0 +1,6 @@
+package com.luv2code.springdemo.practice3;
+
+public interface Coach {
+
+    String getDailyWorkout();
+}
---
+++ src/main/java/com/luv2code/springdemo/practice3/CycleCoach.java
@@ -0,0 +1,8 @@
+package com.luv2code.springdemo.practice3;
+
+public class CycleCoach implements Coach {
+    @Override
+    public String getDailyWorkout() {
+        return "Drive 5km per 10 minutes - do 5 sets";
+    }
+}
