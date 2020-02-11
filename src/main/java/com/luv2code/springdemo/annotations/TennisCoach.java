0d19c938a042296f19f62eb67ba87b1161f82f3b
sergias <sergias@ukr.net>
2020-02-11 18:47:21 +0200

    7. Spring Configuration with Java Annotations - Inversion of Control - 6. Default Component Names - Demo using Default bean IDs

--- src/main/java/com/luv2code/springdemo/annotations/TennisCoach.java
+++ src/main/java/com/luv2code/springdemo/annotations/TennisCoach.java
@@ -2,7 +2,7 @@ package com.luv2code.springdemo.annotations;
 
 import org.springframework.stereotype.Component;
 
-@Component("thatSillyCoach")
+@Component
 public class TennisCoach implements Coach {
 
     @Override
