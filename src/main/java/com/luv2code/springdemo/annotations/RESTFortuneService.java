449647f8a77a930086b2df96688ac35b8cd1f449
sergias <sergias@ukr.net>
2020-02-12 15:11:59 +0200

    8. Spring Configuration with Java Annotations - 13. Qualifiers for Dependency Injection - Write Some Code - Part 1

---
+++ src/main/java/com/luv2code/springdemo/annotations/RESTFortuneService.java
@@ -0,0 +1,12 @@
+package com.luv2code.springdemo.annotations;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RESTFortuneService implements FortuneService {
+
+    @Override
+    public String getFortune() {
+        return null;
+    }
+}
