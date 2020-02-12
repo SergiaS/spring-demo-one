49b50108c9c17f06211b7a27b2c435ae51a5dd6b
sergias <sergias@ukr.net>
2020-02-12 10:41:36 +0200

    8. Spring Configuration with Java Annotations - Dependency Injection - 3. Constructor Injection - Write Some Code - Part 1: I - Define the dependency interface and class

--- src/main/java/com/luv2code/springdemo/annotations/Coach.java
+++ src/main/java/com/luv2code/springdemo/annotations/Coach.java
@@ -3,4 +3,6 @@ package com.luv2code.springdemo.annotations;
 public interface Coach {
 
     public String getDailyWorkout();
+
+    public String getDailyFortune();
 }
---
+++ src/main/java/com/luv2code/springdemo/annotations/HappyFortuneService.java
@@ -0,0 +1,12 @@
+package com.luv2code.springdemo.annotations;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HappyFortuneService implements FortuneService {
+
+    @Override
+    public String getFortune() {
+        return "Today ss your lucky day!";
+    }
+}
