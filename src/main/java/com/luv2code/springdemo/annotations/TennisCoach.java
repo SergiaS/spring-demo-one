cc474165e6a33159cbc23f21e59480f57d26afd8
sergias <sergias@ukr.net>
2020-02-12 12:26:18 +0200

    8. Spring Configuration with Java Annotations - 8. Method Injection

--- src/main/java/com/luv2code/springdemo/annotations/TennisCoach.java
+++ src/main/java/com/luv2code/springdemo/annotations/TennisCoach.java
@@ -13,10 +13,9 @@ public class TennisCoach implements Coach {
         System.out.println(">> TennisCoach: inside default constructor");
     }
 
-    // define a setter method
     @Autowired
-    public void setFortuneService(FortuneService fortuneService) {
-        System.out.println(">> TennisCoach: inside setFortuneService");
+    public void domSMTHCrazyStuff(FortuneService fortuneService) {
+        System.out.println(">> TennisCoach: inside domSMTHCrazyStuff() method");
         this.fortuneService = fortuneService;
     }
 
