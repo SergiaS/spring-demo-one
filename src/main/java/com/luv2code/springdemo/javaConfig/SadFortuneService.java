9969a4c739f2f685de75151e755921ef1c49909f
sergias <sergias@ukr.net>
2020-02-14 18:01:37 +0200

    10. Spring Configuration with Java Code (no xml) - 5. Defining Spring Beans with Java Code (no xml) - Write Some Code - Part 1

--- src/main/java/com/luv2code/springdemo/javaConfig/SadFortuneService.java
+++ src/main/java/com/luv2code/springdemo/javaConfig/SadFortuneService.java
@@ -2,9 +2,8 @@ package com.luv2code.springdemo.javaConfig;
 
 public class SadFortuneService implements FortuneService {
 
-	@Override
-	public String getFortune() {
-		return "Today is a sad day";
-	}
-
+    @Override
+    public String getFortune() {
+        return "Today is a sad day";
+    }
 }
