ac4836f7c4fc2811a17b34a9792ac56f49cfe019
sergias <sergias@ukr.net>
2020-03-05 19:03:00 +0200

    16. Spring MVC Form Validation - Validating Number Ranges and Regular Expressions - 8. How to Handle String input for Integer Fields - Deep Dive

--- src/main/java/com/luv2code/springdemo/mvc/CustomerController.java
+++ src/main/java/com/luv2code/springdemo/mvc/CustomerController.java
@@ -41,6 +41,10 @@ public class CustomerController {
 
         System.out.println("Last name: |" + theCustomer.getLastName() + "|");
 
+        System.out.println("Binding result: " + theBindingResult);
+
+        System.out.println("===========================");
+
         if (theBindingResult.hasErrors()){
             return "customer-form";
         } else {
